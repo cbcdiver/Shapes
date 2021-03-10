075c0f9660d718c9107755be80fce106fa153279
Chris Chadillon <chris@Chadillon.com>
2021-03-10 17:38:01 -0500

    Added the LineSegments Class

---
+++ Shapes/Classes/LineSegments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class LineSegments
+    {
+        public List<Line> theList = new List<Line>();
+        public LineSegments() {}
+        
+        public void addASegment(Line lineToAdd)
+        {
+            if ( this.theList.Count == 0 ) {
+                theList.Add(lineToAdd);
+            }
+            else
+            {
+                Line lastItem = this.theList[this.theList.Count - 1];
+                if (( lastItem.x2 == lineToAdd.x )
+                    && ( lastItem.y2 == lineToAdd.y))
+                {
+                    theList.Add(lineToAdd);
+                }
+                else
+                {
+                    Console.WriteLine("Can not add line!");
+                }
+            }
+        }
+    }
+}
